e9c1cb31c67b947b98a09d823fe36708f1d642aa
Dmitry Bugaev <bugaefff@gmail.com>
2023-05-01 21:44:20 +0300

    fix run dbt

--- extract-inject-prefect-docker/cloud_run_dbt_script.py
+++ extract-inject-prefect-docker/cloud_run_dbt_script.py
@@ -2,7 +2,18 @@ from prefect import flow, task
 from dbt.cli.main import dbtRunner, dbtRunnerResult
 
 @task(log_prints=True)
-def invoke_dbt():
+def invoke_dbt_seed():
+    # initialize
+    dbt = dbtRunner()
+    # create CLI args as a list of strings
+    cli_args = ['seed', '--project-dir', '/dbt', '--profiles-dir', '/root/.dbt']
+    # run the command
+    res: dbtRunnerResult = dbt.invoke(cli_args)
+
+    return res
+
+@task(log_prints=True)
+def invoke_dbt_run():
     # initialize
     dbt = dbtRunner()
     # create CLI args as a list of strings
@@ -25,5 +36,7 @@ def print_result(res : dbtRunnerResult):
 
 @flow(log_prints=True)
 def cloud_run_dbt_flow():
-    res = invoke_dbt()
+    res = invoke_dbt_seed()
+    print_result(res)
+    res = invoke_dbt_run()
     print_result(res)
