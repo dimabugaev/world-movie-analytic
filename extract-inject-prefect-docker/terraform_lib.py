7bfb8b20705de536c70f05c2289cdbc28e9ddcc5
Dmitry Bugaev <bugaefff@gmail.com>
2023-05-01 11:24:49 +0300

    ee

---
+++ extract-inject-prefect-docker/terraform_lib.py
@@ -0,0 +1,16 @@
+import json
+import python_terraform as terraform
+
+# создаем объект Terraform
+tf = terraform.Terraform()
+
+# вызываем команду "output" в Terraform и сохраняем результат в переменную
+output = tf.output()
+print(output)
+# парсим JSON-строку в словарь Python
+#output_dict = json.loads(output)
+#print(output_dict)
+# # получаем значение конкретного output по имени
+# output_value = output_dict['output_name']['value']
+
+# print(output_value)
