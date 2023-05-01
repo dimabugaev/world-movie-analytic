f29726b33c094d9e58e3c41516ef084fa861396e
Dmitry Bugaev <bugaefff@gmail.com>
2023-05-01 21:36:22 +0300

    fix dbt run

--- extract-inject-prefect-docker/cloud_run_dbt_script.py
+++ extract-inject-prefect-docker/cloud_run_dbt_script.py
@@ -6,7 +6,7 @@ def invoke_dbt():
     # initialize
     dbt = dbtRunner()
     # create CLI args as a list of strings
-    cli_args = ['run', '--project-dir', '/dbt']
+    cli_args = ['run', '--project-dir', '/dbt', '--profiles-dir', '/root/.dbt']
     # run the command
     res: dbtRunnerResult = dbt.invoke(cli_args)
 
